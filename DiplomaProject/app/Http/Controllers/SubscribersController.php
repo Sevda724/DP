3a31ba6f7a502c3d2297965a8bc1a21ca8051a9c
Sevda724 <75569199+Sevda724@users.noreply.github.com>
2023-05-14 16:26:41 +0600

    Update SubscribersController.php
    added comments

--- DiplomaProject/app/Http/Controllers/SubscribersController.php
+++ DiplomaProject/app/Http/Controllers/SubscribersController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class SubscribersController extends Controller
 {
+    //displaying subscribers in th eadmin part of the project.
     public function index() {
 
         $subscribers = DB::table('subscribers')->get()->reverse();
@@ -22,15 +23,16 @@ class SubscribersController extends Controller
         return redirect()->to(url()->previous())->withFragment('subs');
     }
 
+    //adding new subscribers to db and sending info about new subscriber to the company
     public function storeSubscribers(Request $request)
     {
-
+        //validation rules and validation messages
         $validator = Validator::make($request->all(), [
             'email' => 'required|email'], [
             'email.required' => __('local.Please enter your email address.'),
             'email.email' => __('local.Please enter a valid email address.'),
         ]);
-
+        //This method is crucial for redirecting with parameters after a failed validation process.
         if ($validator->fails()) {
         return redirect()
         ->back()
@@ -41,15 +43,15 @@ class SubscribersController extends Controller
         } 
     
         $validator->validate();
-
+            //This code snippet checks if a user is already subscribed, preventing them from subscribing again.
             if (Subscriber::where('email', $request->input('email'))->exists()) {
                 return redirect()->back()->with('status_bad', __('local.You are already subscribed!'))->withFragment('subs');
             }
 
             $input = $request->all();
-
+            // adding input to db
             Subscriber::create($input);
-
+            //sending message about new subscriber to the company
             \Mail::send('newSubscriberMail', array(
                 'email' => $input['email'],
             ), function($message) use ($request){
@@ -58,11 +60,8 @@ class SubscribersController extends Controller
             });
 
             return redirect()->to(url()->previous())->withFragment('subs')->with('status_good', __('local.Thanks for subscribing!'));
-        /*} else {
-                 return redirect()->back()->with('status_bad', __('local.Please fill out the field!'))->withFragment('subs');;
-        }*/
     }
-
+    //deleting subscriber in the admin part of the project
     public function destroy(Subscriber $subscriber)
     {
         $subscriber->delete();
